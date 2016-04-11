16e0fbc4a3ea40feba3d8db5f9dc1f93692422a8
Joris Röling <joris@bonboa.com>
2016-04-11 18:07:17 +0200

    stuff

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'meteor-scrolling',
+  name: 'jorisroling:scrolling',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Meteor implementation of jquery.scrolling',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jorisroling/meteor-scrolling.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,12 +13,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
+  api.addFiles('lib/scrolling/jquery.scrolling.js','client');
+  api.addFiles('lib/infinitscroll/jquery.infinitescroll.js','client');
   // api.addFiles('meteor-scrolling.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('meteor-scrolling');
-  // api.addFiles('meteor-scrolling-tests.js');
-});
